ccb1bebebccbb1e815004b1c5cd5a5e3cb135607
Naveen kumar <98445817+Naveen2334@users.noreply.github.com>
2024-03-04 23:52:48 +0530

    Time: 41 ms (5.07%) | Memory: 45.2 MB (21.57%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstUniqChar(String s) {
+       //construction of a hashtable
+        //key--unique characters in the string
+        //value--frequency of each character in the string
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+            else{
+                map.put(s.charAt(i),1);
+            }
+        }
+        //scan the entire hash table and get the index of non repeating character
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i))==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
