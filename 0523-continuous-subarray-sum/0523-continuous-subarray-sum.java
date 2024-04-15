52691904ed9e2056e1ca7bf61e9e36eac0ebe058
Naveen kumar <98445817+Naveen2334@users.noreply.github.com>
2024-04-16 00:57:06 +0530

    Time: 22 ms (79.13%), Space: 57.5 MB (70.22%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+   HashMap<Integer,Integer> hm = new HashMap<>();
+      hm.put(0,0);
+              int sum = 0;
+     
+        for( int  i = 0;i < nums.length; i++){
+            int num = nums[i];
+            sum+=num;
+      
+            if(hm.containsKey(sum%k)){
+                if(hm.get(sum%k)  < i )
+                    return true;
+            }else
+                hm.put(sum%k, i+1);
+            
+        }
+  
+
+      return false;
+    }
+}
